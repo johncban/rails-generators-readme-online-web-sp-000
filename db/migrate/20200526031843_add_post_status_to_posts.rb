06704926738a60b387513b845abfdd569171e31f
Juan Carlo Banayo <johncban@gmail.com>
2020-05-26 03:27:37 +0000

    Automatically backed up by Learn

--- db/migrate/20200526031843_add_post_status_to_posts.rb
+++ db/migrate/20200526031843_add_post_status_to_posts.rb
@@ -1,6 +1,6 @@
 class AddPostStatusToPosts < ActiveRecord::Migration[5.0]
   def change
     add_column :posts, :post_status, :boolean
-    add_column :posts, :post_status, :boolean
+    add_column :posts, :post_status, :string
   end
 end
