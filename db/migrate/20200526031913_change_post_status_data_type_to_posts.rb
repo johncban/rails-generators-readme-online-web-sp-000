89f9ef69a87051e0c000b6875811bb21d2ddc07f
Juan Carlo Banayo <johncban@gmail.com>
2020-05-26 03:29:30 +0000

    Automatically backed up by Learn

--- db/migrate/20200526031913_change_post_status_data_type_to_posts.rb
+++ db/migrate/20200526031913_change_post_status_data_type_to_posts.rb
@@ -1,4 +1,4 @@
-class ChangePostStatusDataTypeToPosts < ActiveRecord::Migration[5.0]
+class ChangePostStatusDataTypeToPosts < ActiveRecord::Migration
   def change
     change_column :posts, :post_status, :string
   end
